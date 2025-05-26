249aa5d13c6d4cc7b680f6d35b36db6666b03288
VAKHULA <andrii.vakhula@gmail.com>
2025-05-26 21:04:19 +0200

    fix domain

--- app/api/proxy/[...slug]/route.js
+++ app/api/proxy/[...slug]/route.js
@@ -3,7 +3,7 @@ import { cookies } from 'next/headers'
 export async function GET(req) {
   const cookieStore = await cookies()
   const cookie = cookieStore.get('my-http-only-cookie')
-  const proxyResp = await fetch(`${process.env.VERCEL_URL || 'http://localhost:3000'}/api/data`, { headers: { authorization: cookie?.value } })
+  const proxyResp = await fetch(`${process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'}`, { headers: { authorization: cookie?.value } })
   const data = await proxyResp.json()
 
   return new Response(JSON.stringify({ data }), {
