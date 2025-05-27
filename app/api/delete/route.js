8056499758f46e12fde44f967503f34124d33675
VAKHULA <andrii.vakhula@gmail.com>
2025-05-27 21:48:11 +0200

    upd

--- app/api/delete/route.js
+++ app/api/delete/route.js
@@ -3,7 +3,7 @@ import { cookies } from 'next/headers'
 export async function GET() {
   const cookieStore = cookies()
   cookieStore.delete('my-http-only-cookie')
-
+  cookieStore.delete('my-http-only-cookie-refresh')
   return new Response(JSON.stringify({ message: "ok" }), {
     status: 200,
     headers: { "Content-Type": "application/json" },
