ea1d07b51436d591d751afa827761149cdfdc357
vakhula <andrii.vakhula@gmail.com>
2025-05-27 08:55:34 +0200

    styles

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+        ></link>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
